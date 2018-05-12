05ec7099f9a6b4be81c72782f735a1762dc8439d
Hamza Ghani <h.u.f.ghani@gmail.com>
2018-05-12 18:30:03 +0100

    hope this works

---
+++ app/src/main/java/io/github/hufghani/ntsp_uom/ui/activity/MainActivity.java
@@ -0,0 +1,4 @@
+package io.github.hufghani.ntsp_uom.ui.activity;
+
+public class MainActivity {
+}
---
+++ app/src/main/java/io/github/hufghani/ntsp_uom/ui/fragment/AlgorithmStepFragment.java
@@ -0,0 +1,4 @@
+package io.github.hufghani.ntsp_uom.ui.fragment;
+
+public class AlgorithmStepFragment {
+}
